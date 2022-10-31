0c4de18855407e4fb0392374d06049ac20d92a9b
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2022-10-31 21:41:16 +0530

    Time: 2 ms (82.01%), Space: 42.7 MB (26.21%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n= word.length();
+        int caps=0;
+        for(int i=0;i<n;i++)
+            if(word.charAt(i)-'A'<26)
+                caps++;
+        
+        if(caps==n) return true;
+        else if(caps==1 && word.charAt(0)-'A'<26) return true;
+        else if(caps==0)  return true;
+        else return false;  
+    }
+}
