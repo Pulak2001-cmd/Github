e73d19ae6b3cb1221f24a3e591253b9c9226f259
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2022-11-04 19:52:29 +0530

    Time: 1591 ms (5.01%), Space: 371.7 MB (5.83%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,12 +1,41 @@
-class Solution:
-    def removeDuplicates(self, s: str, k: int) -> str:
-        stack = []
-        for i in s:
-            if stack and i == stack[-1][0]:
-                stack[-1][1] += 1
-                if stack[-1][1] == k:
-                    stack.pop()
-            else:
-                stack.append([i, 1])
-        return "".join(map(lambda x: x[0] * x[1], stack))
-                
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<List<Integer>> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            int x = s.charAt(i)-'a';
+            if(st.empty()){
+                List<Integer> l = new ArrayList<>();
+                l.add(x);
+                l.add(1);
+                st.push(l);
+            } else if(st.peek().get(0) == x){
+                int y = st.peek().get(1);
+                if (y+1 == k){
+                    st.pop();
+                } else {
+                    st.peek().set(1, y+1);
+                }
+            } else {
+                List<Integer> l = new ArrayList<>();
+                l.add(x);
+                l.add(1);
+                st.push(l);
+            }
+        }
+        String ans = "";
+        while(!st.empty()){
+            List<Integer> cur=st.pop();
+      
+            char currchar = (char)(cur.get(0)+97);
+            int currfreq=cur.get(1);
+
+            while(currfreq>0)
+            {
+                ans=ans+currchar;
+                currfreq--;
+            }
+        }
+        StringBuilder sb = new StringBuilder(ans);
+        return sb.reverse().toString();
+    }
+}
