24fcab313f08b603634b5fcd6557c50f8a4cda4d
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2022-11-07 00:15:49 +0530

    Time: 1 ms (74.13%), Space: 42.4 MB (6.00%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.java
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -6,27 +6,22 @@ class Solution {
         for(char c:suits){
             set.add(c);
         }
-
         if(set.size()==1){
             return "Flush";
         }
-
         int temp=0;
         for(int i=0;i<ranks.length;i++){
             if(map.containsKey(ranks[i])){
                 int curr=map.get(ranks[i]);
                 temp=Math.max(curr,temp);
             }
-
             map.put(ranks[i], map.getOrDefault(ranks[i],0)+1);
         }
-
         if(temp>=2){
             return "Three of a Kind";
         }else if(temp==1){
             return "Pair";
         }
-
         return "High Card";
     }
 }
