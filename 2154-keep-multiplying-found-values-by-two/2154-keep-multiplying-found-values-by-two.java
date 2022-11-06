4b48e3f95aca24aa4a67ba7cf5314acc9e8e31d2
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2022-11-07 04:09:57 +0530

    Time: 12 ms (9.50%), Space: 44.7 MB (43.64%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,5 +1,23 @@
-class Solution:
-    def findFinalValue(self, nums: List[int], original: int) -> int:
-        while(original in nums):
-            original = original * 2
-        return original
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        if(binarySearch(nums, original)) return findFinalValue(nums, original*2);
+        return original;
+    }
+    public boolean binarySearch(int[] nums, int target){
+        int i=0;
+        int l = nums.length-1;
+        while(i<=l){
+            int mid = i +(l-i)/2;
+            if (nums[mid] == target) return true;
+            else {
+                if (nums[mid] < target){
+                    i=mid+1;
+                } else{
+                    l = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
