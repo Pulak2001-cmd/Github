d88f787c9dc98b989b57ec9f1f0785c400e58754
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2022-11-08 00:45:34 +0530

    Time: 1 ms (82.63%), Space: 42.5 MB (18.05%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int r=0,left=nums[0];
+        int ans=0,left=nums[0];
         for(int i=1;i<nums.length-1;i++){
             if(left<nums[i] && nums[i]>nums[i+1] || left>nums[i] && nums[i]<nums[i+1]){
-                r++;
+                ans++;
                 left=nums[i];
             }
         }
-        return r;
+        return ans;
     }
 }
