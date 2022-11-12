94e6022fe22951aeb54dbe8f7c3488d6017652bf
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2022-11-12 20:49:16 +0530

    Time: 424 ms (17.41%), Space: 14.3 MB (9.23%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -0,0 +1,5 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        subs = [s[i:j] for i in range(len(s)) for j in range(i+1, len(s)+1)]
+        nice = [sub for sub in subs if set(sub)==set(sub.swapcase())]
+        return max(nice, key=len, default="")
